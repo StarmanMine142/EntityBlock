2d0f2a80bc435576597b13cfd251cbdc0997e337
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-12-07 07:08:21 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+	id("fabric-loom") version "1.14-SNAPSHOT"
+	id("maven-publish")
+}
+
+java.sourceCompatibility = JavaVersion.VERSION_21
+java.targetCompatibility = JavaVersion.VERSION_21
+
+version = "${project.property("mod_version")}"
+
+base.archivesName.set(project.property("archives_base_name") as String)
+
+repositories {
+
+}
+
+loom {
+	splitEnvironmentSourceSets()
+
+	mods {
+		create("entity_block") {
+			sourceSet(sourceSets["main"])
+			sourceSet(sourceSets["client"])
+		}
+	}
+
+}
+
+dependencies {
+	minecraft("com.mojang:minecraft:${property("minecraft_version")}")
+	mappings(loom.officialMojangMappings())
+	modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
+
+	// Fabric API
+	modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
+}
+
+tasks {
+	processResources {
+		inputs.property("version", project.property("mod_version"))
+
+		filesMatching("fabric.mod.json") {
+			expand(
+				mutableMapOf(
+					"version" to project.property("mod_version")
+				)
+			)
+		}
+	}
+
+	withType<JavaCompile> {
+		options.release.set(21)
+	}
+
+	java {
+		withSourcesJar()
+	}
+
+	jar {
+		from("LICENSE") {
+			rename {"${it}_${base.archivesName.get()}"}
+		}
+	}
+}
+
+// configure the maven publication
+publishing {
+	publications {
+		create<MavenPublication>("mavenJava") {
+			from(components["java"])
+		}
+	}
+
+	// See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
+	repositories {
+		// Add repositories to publish to here.
+		// Notice: This block does NOT have the same function as the block in the top level.
+		// The repositories here will be used for publishing your artifact, not for
+		// retrieving dependencies.
+	}
+}
---
+++ src/client/java/net/pneumono/entityblock/EntityBlockClient.java
@@ -0,0 +1,10 @@
+package net.pneumono.entityblock;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EntityBlockClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
